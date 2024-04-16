ca303ec3b80e1a21fe820dc514206cf6e369acaa
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-16 18:54:03 +0100

    adding the token to the signup to create the token in the server when he gets the req of the signup

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -1,4 +1,11 @@
 import { User } from "../models/userModel.mjs";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, {
+    expiresIn: "3d",
+  });
+};
 
 //! login a user
 export const loginUser = async (req, res) => {};
@@ -9,7 +16,11 @@ export const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json(user);
+
+    // Create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({token, user});
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
