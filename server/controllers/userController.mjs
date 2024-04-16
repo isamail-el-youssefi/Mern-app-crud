e9e65802e41784b9df89baf4b921bb38eaf15b0e
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-16 17:24:57 +0100

    sign up with static signup method with hashing the password

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -1,9 +1,16 @@
+import { User } from "../models/userModel.mjs";
+
 //! login a user
-export const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
-};
+export const loginUser = async (req, res) => {};
 
 //! signup a user
 export const signupUser = async (req, res) => {
-  res.json({ mssg: "signup user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
--- server/models/userModel.mjs
+++ server/models/userModel.mjs
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
   email: {
@@ -11,5 +12,21 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+//? Static signup method
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already exist");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedpassword = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hashedpassword });
+
+  return user;
+};
 
 export const User = mongoose.model("User", userSchema);
