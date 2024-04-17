5aae627103f51273d19b0fdbb1956cc427efb0e0
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-17 13:00:30 +0100

    adding the login and the sign up state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+          <Routes>
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sihn up</h3>
+
+      <label>Email address:</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+
+      <label>Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+
+      <button>Sign up</button>
+    </form>
+  );
+}
